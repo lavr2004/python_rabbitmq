c696fdb8db53daa624665f1172b95890768b5840
a.ihnatovicz <andrei.ihnatovicz@vacancysoft.com>
2020-04-13 23:13:12 +0200

    20200413UPDATE

--- rabbitmq_receiver.py
+++ rabbitmq_receiver.py
@@ -1,24 +1,54 @@
+#coding:utf-8
+#example of receiver module for rabbitmq realized in composition design pattern
+
 try:
     import pika
 except Exception as e:
-    print('Pika module dont imported')
+    print(f'ER - pika module missing: {e}')
 
-connection_obj = pika.BlockingConnection(
-    pika.ConnectionParameters(host='localhost')
-)
+class Singleton(type):
+    '''Singleton design pattern realised like metaclass'''
+    _instance = {}
+    def __call__(self, *args, **kwargs):
+        if self not in self._instance:
+            #self._instance[self] = super(RabbitMQ_singleton, self).__call__(*args, **kwargs)
+            self._instance.update({self:super(Singleton, self).__call__(*args, **kwargs)})
+            return self._instance.get(self)
 
-channel_obj = connection_obj.channel()
+class Rabbitmqreceiverconfig():
+    def __init__(self, hostpath_str = 'localhost', queuetitle_str = 'hello'):
+        self.hostpath_str = hostpath_str
+        self.queuetitle_str = queuetitle_str
 
-channel_obj.queue_declare(queue='hello')
+class Rabbitmqreceiver(metaclass=Singleton):
 
-def callback_fc(channel_obj, method_obj, properties_obj, body_obj):
-    print(f"[x] Received {body_obj}")
+    def __init__(self, rabbitmqreceiverconfig_obj):
+        self.rmqreceivercfg_obj = rabbitmqreceiverconfig_obj
+        self._connection_obj = pika.BlockingConnection(pika.ConnectionParameters(host=self.rmqreceivercfg_obj.hostpath_str))
+        self._channel_obj = self._connection_obj.channel()
+        self.tmp_obj = self._channel_obj.queue_declare(self.rmqreceivercfg_obj.queuetitle_str)
 
-channel_obj.basic_consume(
-    queue='hello',
-    on_message_callback=callback_fc,
-    auto_ack=True
-)
+    def _callback_fc(self, channel_obj, method_obj, properties_obj, body_obj):
+        '''method for locking what objects got from queue'''
+        print(f"[x] Received channel_obj: {channel_obj}")
+        print(f"[x] Received method_obj: {method_obj}")
+        print(f"[x] Received properties_obj: {properties_obj}")
+        print(f"[x] Received body_obj: {body_obj}")
+        print('='*100)
 
-print('[x] Waiting for messages. To exit press CTRL+C')
-channel_obj.start_consuming()
+    def process_fc(self, isauto_ack = True):
+        ''' main operation of class'''
+        self._channel_obj.basic_consume(queue=self.rmqreceivercfg_obj.queuetitle_str, on_message_callback=self._callback_fc, auto_ack=isauto_ack)
+        print('[*] Waiting for messages. To exit press CTRL+C')
+        self._channel_obj.start_consuming()
+
+if __name__ == '__main__':
+    hostpath_str = 'localhost'
+    queuetitle_str = 'hello'
+    print(f'OK - start testing rabbitMQ sender with such paramters:\ntitle of queue on server = {queuetitle_str}\nserver path = {hostpath_str}')
+    try:
+        rabbitmqreceivercfg_obj = Rabbitmqreceiverconfig(hostpath_str, queuetitle_str)
+        rabbitmqreceiver_obj = Rabbitmqreceiver(rabbitmqreceivercfg_obj)
+        rabbitmqreceiver_obj.process_fc()
+    except Exception as e:
+        print(e)
--- rabbitmq_sender.py
+++ rabbitmq_sender.py
@@ -1,19 +1,56 @@
+#coding:utf-8
+#example of sender module for rabbitmq realized in composition design pattern
+
+import datetime
 try:
     import pika
 except Exception as e:
-    print('Pika module missing')
+    print(f'ER - pika module missing: {e}')
 
-connection_obj = pika.BlockingConnection(
-    pika.ConnectionParameters(host='localhost')
-)
+class Singleton(type):
+    '''Singleton design pattern realised like metaclass'''
+    _instance = {}
+    def __call__(self, *args, **kwargs):
+        if self not in self._instance:
+            #self._instance[self] = super(RabbitMQ_singleton, self).__call__(*args, **kwargs)
+            self._instance.update({self:super(Singleton, self).__call__(*args, **kwargs)})
+            return self._instance.get(self)
 
-channel_obj = connection_obj.channel()
+class Rabbitmqsenderconfig(metaclass=Singleton):
+    def __init__(self, queuetitle_str = 'hello', hostpath_str = 'localhost', exchange_str =''):
+        '''queuetitle_str - is title of queue inside of Rabbitmq'''
+        self.queuetitle_str = queuetitle_str
+        self.hostpath_str = hostpath_str
+        self.exchange_str = exchange_str
 
-channel_obj.queue_declare(queue='hello')
 
-channel_obj.basic_publish(exchange='',
-                          routing_key='hello',
-                          body='Hello world RabbitMQ!'
+class Rabbitmqsender():
+    '''organized accordingly composition design pattern, where ser'''
+    def __init__(self, rabbitmqsenderconfig_obj):
+        self.rabbitmqcfg_obj = rabbitmqsenderconfig_obj
+        self._connection_obj = pika.BlockingConnection(pika.ConnectionParameters(host=self.rabbitmqcfg_obj.hostpath_str))
+        self.channel_obj = self._connection_obj.channel()
+        self.channel_obj.queue_declare(queue=self.rabbitmqcfg_obj.queuetitle_str)
+
+    def publishpayload_fc(self, payload_dc):
+        '''method for sending something to queue as a string'''
+        self.channel_obj.basic_publish(exchange=self.rabbitmqcfg_obj.exchange_str,
+                          routing_key=self.rabbitmqcfg_obj.queuetitle_str,
+                          body=str(payload_dc)
                           )
-print("Msg published!")
-connection_obj.close()
+        print(f"Msg published to queue: {payload_dc}")
+
+    def __del__(self):
+        self._connection_obj.close()
+
+if __name__ == '__main__':
+    queuetitle_str = 'hello'
+    hostpath_str = 'localhost'
+    exchange_str = ''
+    print(f'OK - start testing rabbitMQ sender with such paramters:\ntitle of queue on server = {queuetitle_str}\nserver path = {hostpath_str}\nexchange_str = {exchange_str}')
+    try:
+        rabbitmqconfigure_obj = Rabbitmqsenderconfig(queuetitle_str, hostpath_str, exchange_str)
+        rmqsender_obj = Rabbitmqsender(rabbitmqconfigure_obj)
+        rmqsender_obj.publishpayload_fc(payload_dc={"Data":f"Hello payload {datetime.datetime.now()}"})
+    except Exception as e:
+        print(e)
